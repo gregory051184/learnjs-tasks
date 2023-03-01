3e591142ef8291a6f0d8767cdf8f2ec323e1aa01
gregory051184 <126391093+gregory051184@users.noreply.github.com>
2023-03-01 17:56:45 +0400

    Add files via upload

---
+++ task11_Промисы_async_await/ex11.2/ex11.2.1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Можно ли "перевыполнить" промис?
+
+//Что выведет код ниже?
+
+let promise = new Promise(function (resolve, reject) {
+    resolve(1);
+
+    setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(console.log); // => 1, т.к. учитывается только первый вызов
