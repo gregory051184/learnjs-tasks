5de25c7a3845c5e241657e6d79c584cb6475226d
gregory051184 <126391093+gregory051184@users.noreply.github.com>
2023-03-01 18:06:53 +0400

    Add files via upload

---
+++ task14_Разное/ex14.5/ex14.5.2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Проверка, целое ли число
+
+//Напишите функцию isInteger(num), которая возвращает true, если num – целое число, иначе false.
+// Например:
+
+let isInteger = num => {
+    return (num ^ 0) === num;
+}
+
+console.log( isInteger(1) ); // true
+console.log( isInteger(1.5) ); // false
+console.log( isInteger(-0.5) ); // false
